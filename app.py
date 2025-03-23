1e87ea32e04e88333e8bbbd8dd90e769296b5f00
sufiys <shaikhsufiyan22@gmail.com>
2025-03-23 13:34:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 import streamlit as st
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
-from langchain.llms import OpenAI
+from langchain_openai import OpenAI
 from langchain.document_loaders import TextLoader
 
 # Load OpenAI API Key from Streamlit Secrets
